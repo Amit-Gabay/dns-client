5b348d471e13aaebc4c2dd309eb30110c4184f54
Amit Gabay <amitgabay9142@gmail.com>
2022-05-31 12:03:21 +0300

    Skeleton

---
+++ nsclient/nsclient.c
@@ -0,0 +1,14 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "utils.h"
+#include <winsock2.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <Ws2tcpip.h>
+
+
+int main(int argc, char** argv)
+{
+	return 0; // TODO
+}
